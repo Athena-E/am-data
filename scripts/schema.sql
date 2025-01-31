e5f2a5a7af7154029b3d81abb9c1792898e27e57
Athena-E <athena.eng2004@gmail.com>
2025-01-31 16:06:17 +0000

    update db schema

--- scripts/schema.sql
+++ scripts/schema.sql
@@ -4,5 +4,6 @@ CREATE TABLE IF NOT EXISTS sensor_data (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     timestamp TEXT NOT NULL,
     temperature REAL NOT NULL,
-    humidity REAL NOT NULL
+    humidity REAL NOT NULL,
+    co2 REAL NOT NULL
 );
