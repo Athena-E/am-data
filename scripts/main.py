ebd0de6bb2b969ce1f81045055436440dd26b7b4
Oliver Greenwood <greenwood672.dev@gmail.com>
2025-02-13 11:58:32 +0000

    added arguments

--- scripts/main.py
+++ scripts/main.py
@@ -7,24 +7,37 @@ from scripts.data_loader import get_day_data
 
 from scripts.clean_data.clean import clean
 
+
+import argparse
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--add-db", help="Add database?", action=argparse.BooleanOptionalAction)
+parser.add_argument("--clean-data", help="Clean Database?", action=argparse.BooleanOptionalAction)
+
+args = parser.parse_args()
+
+
 def main():
+    
+    if (args.add_db):
 
-    # init_db()
-    # print("Database Initialise!")
+        init_db()
+        print("Database Initialise!")
 
-    # PREPROCESSED_DIR = './data/preprocessed'
+        PREPROCESSED_DIR = './data/preprocessed'
 
-    # # get all preprocessed day data files
-    # data_files = [f for f in os.listdir(PREPROCESSED_DIR) if os.path.isfile(os.path.join(PREPROCESSED_DIR, f))]
+        # get all preprocessed day data files
+        data_files = [f for f in os.listdir(PREPROCESSED_DIR) if os.path.isfile(os.path.join(PREPROCESSED_DIR, f))]
 
-    # all_extracted = []
-    # for f in data_files:
-    #     all_extracted += get_day_data(f)
+        all_extracted = []
+        for f in data_files:
+            all_extracted += get_day_data(f)
 
-    # insert_list_sensor_data(all_extracted)
-    # print("Records inserted")
+        insert_list_sensor_data(all_extracted)
+        print("Records inserted")
 
-    clean()
+    if (args.clean_data):
+        clean()
 
 
 if __name__ == "__main__":
