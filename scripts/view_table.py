93f8bd743e5c93fb9e205d0fad37d9679e903817
Sunny Cheung <hyc55@cam.ac.uk>
2025-02-13 14:10:31 +0000

    update view_table.py to include coordinate data

--- scripts/view_table.py
+++ scripts/view_table.py
@@ -1,33 +1,26 @@
-import sqlite3
-import os 
-import sys
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+from scripts.db.utils import get_db
 
-from scripts.db import DATABASE
 
 def display_table():
     # display all records in sensor_table
-    conn = sqlite3.connect(DATABASE)
-    conn.row_factory = sqlite3.Row
-    cursor = conn.cursor()
+    db = get_db()
 
-    print('sensor data:')
-    
-    cursor.execute("SELECT * FROM sensor_data")
-    
-    rows = cursor.fetchall()
-    for row in rows:
-        print({col: row[col] for col in row.keys()})
-    
-    print('coordinates:')
+    print("sensor data:")
 
-    cursor.execute("SELECT * FROM coordinates")
+    cursor = db.execute("SELECT * FROM sensor_data")
 
     rows = cursor.fetchall()
     for row in rows:
         print({col: row[col] for col in row.keys()})
 
-    conn.close()
+    print("coordinates:")
+
+    cursor = db.execute("SELECT * FROM coordinates")
+
+    rows = cursor.fetchall()
+    for row in rows:
+        print({col: row[col] for col in row.keys()})
+
 
 if __name__ == "__main__":
     display_table()
