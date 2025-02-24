76c1a6f527e3ac939968251cbded0e7d81776fb3
Oliver Greenwood <greenwood672.dev@gmail.com>
2025-02-24 12:23:23 +0000

    Occupency totally works

--- scripts/data_loader/import_occupency.py
+++ scripts/data_loader/import_occupency.py
@@ -1,23 +1,24 @@
 from scripts.db.utils import get_db
 import os
 import json
+from tqdm import tqdm
 
 
 def parse(raw):
     data = json.loads(raw)
     payload = {
         "crowd_count": data["payload_cooked"]["crowdcount"],
-        "occupency_filled": data["payload_cooked"]["occupency_filled"],
+        "occupency_filled": data["payload_cooked"]["occupancy_filled"],
         "timestamp": data["acp_ts"],
     }
-    payload["seats"] = payload["payload_cooked"]["seats_occupied"].values()
+    payload["seats"] = data["payload_cooked"]["seats_occupied"].values()
     return payload
 
 def get_next_file(folder):
     for f in os.listdir(folder):
         path = os.path.join(folder, f)
-        if os.path.isfile(f):
-            yield f
+        if os.path.isfile(path):
+            yield path
         elif os.path.isdir(path):
             yield from get_next_file(path)
 
@@ -27,32 +28,47 @@ def import_occupency():
 
     gen = get_next_file(folder)
     conn = get_db()
+    cursor = conn.cursor()
 
-    for filename in gen:
+    for filename in tqdm(gen):
         with open(filename) as f:
             for line in f.readlines():
-                payload = parse(line)
+                try:
+                    payload = parse(line)
 
-                conn.execute(
+                    assert all(
+                        map(
+                            lambda seat: all(
+                                ["seat_id" in seat, "seat_confidence" in seat, "model_confidence" in seat]
+                            ), payload["occupency_filled"]
+                        )
+                    ) or len(payload["occupency_filled"]) == 0
+
+                    assert all(["crowd_count" in payload, "occupency_filled" in payload, "timestamp" in payload])
+
+                except:
+                    pass
+
+                cursor.execute(
                     """
                     INSERT OR REPLACE INTO occupency
                     (crowdcount, occupency_filled, timestamp)
                     VALUES (?, ?, ?)
-                    """, (payload["crowd_count"], payload["occupency_filled", "timestamp"])
+                    """, (payload["crowd_count"], payload["occupency_filled"], payload["timestamp"])
                 )
 
-                ref = conn.lastrowid
+                ref = cursor.lastrowid
 
                 for seat in payload["seats"]:
-                    conn.execute(
+                    cursor.execute(
                         """
                         INSERT OR REPLACE INTO seats
                         (seat, occupency_id, seat_confidence, model_confidence)
                         VALUES (?, ?, ?, ?)
                         """,
-                        (seat["seat"], ref, seat["seat_confidence"], seat["model_confidence"])
+                        (seat["seat_id"], ref, seat["seat_confidence"], seat["model_confidence"])
                     )
-    
+                    
     conn.commit()
     conn.close()
 
